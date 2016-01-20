df6fa5d75bda0ec749189af600205eda6e9bc940
andi <unitygit@andreas-reiff.de>
2016-01-20 11:20:16 +0100

    video 11 MonoSingleton

---
+++ Assets/Script/Systems/MonoSingleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MonoSingleton<T> : MonoBehaviour
+where T : MonoSingleton<T>
+{
+    static T instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject(typeof(T).ToString(), typeof(T)).GetComponent<T>();
+                instance.Init();
+            }
+            return instance;
+        }
+    }
+    public virtual void Init() { }
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this as T;
+        Init();
+    }
+}
